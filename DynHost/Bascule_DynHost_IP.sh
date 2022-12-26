0811a6c4daafc5f24ce6ff8151e35962f0ec1b33
Maniack Crudelis <maniackc_dev@crudelis.fr>
2022-12-26 16:19:19 +0100

    Extract credentials from dynhost script

--- DynHost/Bascule_DynHost_IP.sh
+++ DynHost/Bascule_DynHost_IP.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-HOST=DOMAIN
-LOGIN=LOGIN
-PASSWORD=PASSWORD
+script_dir="$(dirname $(realpath $0))"
+
+source "$script_dir/cred"
 
 IP=$(curl -s http://www.monip.org/ | grep "IP : " | cut -d':' -f2 | cut -d' ' -f2| cut -d'<' -f1)
-python DynHost/ipcheck.py -v -a $IP $LOGIN $PASSWORD $HOST
+python2.7 "$script_dir/ipcheck.py" -v -a $IP $LOGIN $PASSWORD $HOST
